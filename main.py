ec1508f8e6893b205c66595256634678e8f2963e
tomotomo <tomotomobile@gmail.com>
2019-02-26 23:22:08 +0900

    buffer sending

--- main.py
+++ main.py
@@ -8,51 +8,52 @@ import time
 import datetime
 
 import grove_d7s
-import ambient
+from ambient_connector import AmbientConnector
 
 # sensor instance
 sensor = grove_d7s.GroveD7s()
 
-# ambient instance
-try:
-    AMBIENT_CHANNEL_ID = int(os.environ['AMBIENT_CHANNEL_ID'])
-    AMBIENT_WRITE_KEY = os.environ['AMBIENT_WRITE_KEY']
-    CHECK_SPAN = int(os.environ.get('CHECK_SPAN', '30'))
-    am = ambient.Ambient(AMBIENT_CHANNEL_ID, AMBIENT_WRITE_KEY)
-except KeyError:
-    print(KeyError)
-    exit(1)
+# ambientをラップしたAmbientConnectorを使います
+am = AmbientConnector()
+
 
 def main():
     while sensor.isReady() == False:
         print('.')
         time.sleep(1.0)
-    
-    print ("start")
-    
-    while True:
-          time.sleep(1.0)
-          if sensor.isEarthquakeOccuring() == True:
-              eq = sensor.isEarthquakeOccuring()
-              si = sensor.getInstantaneusSI()
-              pga = sensor.getInstantaneusPGA()
-              now = datetime.datetime.today()
-              
-              if si == None and pga == None:
-                  continue
-              payload = {
-                  "d5": eq,
-                  "d1": si,
-                  "d2": pga,
-                  "created": now.strftime("%Y/%m/%d %H:%M:%S")
-              }
-              try:
-                  am.send(payload)
-              except Exception as e:
-                  print(e)
 
-              print(now.strftime("[%Y/%m/%d %H:%M:%S]")
-                        ,"SI=%.1f[Kine]" %si,"PGA=%d[gal]" %pga)
+    print("start")
+
+    while True:
+        # 1秒間隔でデータを取得する
+        time.sleep(1.0)
+        
+    while True:
+        # 10秒のインターバルを設定
+        time.sleep(10)
+        # センサーデータの取得
+        si = sensor.getInstantaneusSI()
+        pga = sensor.getInstantaneusPGA()
+        now = datetime.datetime.today()
+        eq = sensor.isEarthquakeOccuring()
+
+        # センサーの初期化中は値がNoneになるので処理をスキップ
+        if si == None and pga == None:
+            continue
+
+        # Ambientに送信するペイロードを作成
+        payload = {
+            "d5": int(eq),
+            "d1": si,
+            "d2": pga,
+            "created": now.strftime("%Y/%m/%d %H:%M:%S")
+            }
+        # データをバッファーする
+        am.buffer(payload)
+
+        # デバッグ用に送信したデータを標準出力（本当は不要）
+        print(now.strftime("[%Y/%m/%d %H:%M:%S]"),
+                "SI=%.1f[Kine]" % si, "PGA=%d[gal]" % pga)
 
 if __name__ == '__main__':
-  main()
+    main()
